139ffc4d11289096792cb55b44b33b65452c6dbe
Mantas Norvaiša <mntnorv@gmail.com>
2013-06-21 13:53:48 +0300

    Added method to simplify getting HTML

--- wb.c
+++ wb.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <curl/curl.h>
 
 /*
  * Global constants
  */
+
 static const char *URL_HOMEPAGE = "http://wallbase.cc/start";
 static const char *URL_LOGIN = "http://wallbase.cc/user/login";
+static const char *URL_TOPLIST = "http://wallbase.cc/toplist";
+
 static const char *FORMAT_LOGIN = "usrname=%s&pass=%s&nopass_email=Type+in+your+e-mail+and+press+enter&nopass=0&1=1";
 
 /*
@@ -22,7 +26,12 @@ struct curl_response {
 /*
  * Function declarations
  */
-struct curl_slist *login(char *username, char *password);
+
+struct curl_slist *wb_login(char *username, char *password);
+struct curl_slist *wb_get_image_page_urls(char *html_data);
+
+char *curl_get_response(const char *url, char *post_data, struct curl_slist *cookies);
+void curl_add_cookies(CURL *curl, struct curl_slist *cookies);
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, struct curl_response *data);
 char *url_encode(char *str);
@@ -31,15 +40,20 @@ char to_hex(char code);
 /*
  * Main
  */
+
 int main(int argc, char* argv[]) {
 	/* Variables */
 	struct curl_slist *cookies;
+	char *toplist;
 
 	/* Init */
 	curl_global_init(CURL_GLOBAL_ALL);
 
 	/* Main operation */
-	cookies = login("user", "pass");
+	cookies = wb_login("", "");
+	toplist = curl_get_response(URL_TOPLIST, NULL, cookies);
+	puts(toplist);
+	free(toplist);
 
 	/* Cleanup nd return */
 	curl_slist_free_all(cookies);
@@ -54,7 +68,7 @@ int main(int argc, char* argv[]) {
 /* Login to wallbase.cc */
 /* Returns a list of cookies */
 /* IMPORTANT: curl_slist_free_all() the cookies after usage */
-struct curl_slist *login(char *username, char *password) {
+struct curl_slist *wb_login(char *username, char *password) {
 	char *enc_username;
 	char *enc_password;
 	char post_data[256];
@@ -102,15 +116,72 @@ struct curl_slist *login(char *username, char *password) {
 	}
 
 	curl_easy_cleanup(curl);
+	free(response.data);
 	free(enc_username);
 	free(enc_password);
 
 	return cookies;
 }
 
+/* Get wallbase.cc image page URLs from HTML */
+/* IMPORTANT: the returned list must be freed with curl_slist_free_all() */
+struct curl_slist *wb_get_image_page_urls(char *html_data) {
+
+}
+
 /*
  * Helper function implementations
  */
+
+/* Connect to URL with GET or POST requests and return the response. */
+/* If post_data is NULL, then a GET request is issued. */
+/* Uses cookies if cookies is not NULL. */
+/* IMPORTANT: the returned string should be freed with free() */
+char *curl_get_response(const char *url, char *post_data, struct curl_slist *cookies) {
+	CURL *curl;
+	CURLcode res;
+
+	struct curl_response response;
+	response.size = 0;
+	response.data = malloc(4096);
+	if (response.data == NULL) {
+		fprintf(stderr, "Failed to allocate memory.\n");
+		exit(1);
+	}
+	response.data[0] = '\0';
+
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+	if (post_data != NULL) {
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
+	}
+
+	if (cookies != NULL) {
+		curl_add_cookies(curl, cookies);
+	}
+
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK) {
+		fprintf(stderr, "Curl perform failed: %s\n", curl_easy_strerror(res));
+		exit(1);
+	}
+
+	curl_easy_cleanup(curl);
+	return response.data;
+}
+
+/* Add all cookies in a curl_slist (returned by CURLINFO_COOKIELIST) */
+/* to a CURL object */
+void curl_add_cookies(CURL *curl, struct curl_slist *cookies) {
+	struct curl_slist *cookie = cookies;
+	while (cookie != NULL) {
+		curl_easy_setopt(curl, CURLOPT_COOKIELIST, cookie->data);
+		cookie = cookie->next;
+	}
+}
 
 /* Write curl response to a string */
 size_t write_data(void *ptr, size_t size, size_t nmemb, struct curl_response *data) {
@@ -151,14 +222,19 @@ char to_hex(char code) {
 /* Returns a url-encoded version of str */
 /* IMPORTANT: be sure to free() the returned string after use */
 char *url_encode(char *str) {
-	char *pstr = str, *buf = malloc(strlen(str) * 3 + 1), *pbuf = buf;
+	char *pstr = str;
+	char *buf = malloc(strlen(str) * 3 + 1);
+	char *pbuf = buf;
 	while (*pstr) {
-		if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') 
+		if (isalnum((unsigned char)*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') {
 			*pbuf++ = *pstr;
-		else if (*pstr == ' ') 
+		} else if (*pstr == ' ') {
 			*pbuf++ = '+';
-		else 
-			*pbuf++ = '%', *pbuf++ = to_hex(*pstr >> 4), *pbuf++ = to_hex(*pstr & 15);
+		} else {
+			*pbuf++ = '%';
+			*pbuf++ = to_hex(*pstr >> 4);
+			*pbuf++ = to_hex(*pstr & 15);
+		}
 		pstr++;
 	}
 	*pbuf = '\0';
