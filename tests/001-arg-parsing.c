72f022651ef1eb65549ec29fb0eee83f40b7b463
Mantas Norvaiša <mntnorv@gmail.com>
2013-07-02 16:31:21 +0300

    Updated argument parsing test

--- tests/001-arg-parsing.c
+++ tests/001-arg-parsing.c
@@ -5,18 +5,21 @@
 
 struct options options;
 
+/* Unity set up and tear down */
 void setUp() {
 }
 
 void tearDown() {
 }
 
+/* Mock functions */
 void wb_error(const char *format, ...) {	
 }
 
 void wb_error_no_prefix(const char *fomrat, ...) {
 }
 
+/* Functions that return to a known state */
 void resetOptions() {
 	options.username = "";
 	options.password = "";
@@ -41,6 +44,7 @@ void resetOptions() {
 	options.sort_order = WB_SORT_DESCENDING;
 }
 
+/* Tests*/
 void test_parseOpt_aspectRatio_valid() {
 	int res;
 
@@ -109,6 +113,298 @@ void test_parseOpt_color_invalid() {
 	TEST_ASSERT_EQUAL_INT(-1, res);
 }
 
+void test_parseOpt_downloadDir_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('d', "unity", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_STRING("unity", options.dir);
+
+	resetOptions();
+	res = parse_opt('d', ".", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_STRING(".", options.dir);
+}
+
+void test_parseOpt_downloadDir_invalid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('d', "fakedir", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+}
+
+void test_parseOpt_imageNum_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('n', "1", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(1, options.images);
+
+	resetOptions();
+	res = parse_opt('n', "1000", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(1000, options.images);
+}
+
+void test_parseOpt_imageNum_invalid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('n', "0", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('n', "-1", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('n', "0xfakenumber", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+}
+
+void test_parseOpt_password_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('p', "password", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_STRING("password", options.password);
+}
+
+void test_parseOpt_query_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('q', "myquery", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_STRING("myquery", options.query);
+}
+
+void test_parseOpt_resolution_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('r', "1920x1080", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(1920, options.res_x);
+	TEST_ASSERT_EQUAL_INT(1080, options.res_y);
+
+	resetOptions();
+	res = parse_opt('r', "=800x600", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(800, options.res_x);
+	TEST_ASSERT_EQUAL_INT(600, options.res_y);
+	TEST_ASSERT_EQUAL_INT(WB_RES_EXACTLY, options.res_opt);
+}
+
+void test_parseOpt_resolution_invalid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('r', "10", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('r', "0x1080", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('r', "=0x1080", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('r', "1920x0", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('r', "=1920x0", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('r', "numxnum", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+}
+
+void test_parseOpt_sort_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('s', "r", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_RELEVANCE, options.sort_by);
+
+	resetOptions();
+	res = parse_opt('s', "+v", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_VIEWS, options.sort_by);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_ASCENDING, options.sort_order);
+
+	resetOptions();
+	res = parse_opt('s', "-f", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_FAVORITES, options.sort_by);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_DESCENDING, options.sort_order);
+
+	resetOptions();
+	res = parse_opt('s', "d", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_DATE, options.sort_by);
+
+	resetOptions();
+	res = parse_opt('s', "relevance", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_RELEVANCE, options.sort_by);
+
+	resetOptions();
+	res = parse_opt('s', "+views", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_VIEWS, options.sort_by);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_ASCENDING, options.sort_order);
+
+	resetOptions();
+	res = parse_opt('s', "-favorites", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_FAVORITES, options.sort_by);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_DESCENDING, options.sort_order);
+
+	resetOptions();
+	res = parse_opt('s', "date", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_SORT_DATE, options.sort_by);
+}
+
+void test_parseOpt_sort_invalid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('s', "randomstr", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('s', "favs", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('s', "+favorites+", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('s', "v+", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('s', "d-", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+}
+
+void test_parseOpt_toplist_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('t', "1d", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_1D, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "3d", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_3D, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "1w", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_1W, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "2w", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_2W, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "1m", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_1M, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "2m", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_2M, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "3m", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_3M, options.toplist);
+
+	resetOptions();
+	res = parse_opt('t', "0", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_TOPLIST_ALL_TIME, options.toplist);
+}
+
+void test_parseOpt_toplist_invalid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('t', "randomstr", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('t', "2d", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+
+	resetOptions();
+	res = parse_opt('t', "3w", &options);
+	TEST_ASSERT_EQUAL_INT(-1, res);
+}
+
+void test_parseOpt_username_valid() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('u', "whatuser", &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_STRING("whatuser", options.username);
+}
+
+void test_parseOpt_flags() {
+	int res;
+
+	resetOptions();
+	res = parse_opt('S', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_PURITY_SFW, options.purity & WB_PURITY_SFW);
+
+	res = parse_opt('K', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_PURITY_SKETCHY, options.purity & WB_PURITY_SKETCHY);
+
+	res = parse_opt('N', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_PURITY_NSFW, options.purity & WB_PURITY_NSFW);
+
+	resetOptions();
+	res = parse_opt('G', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_BOARD_GENERAL, options.boards & WB_BOARD_GENERAL);
+
+	res = parse_opt('A', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_BOARD_ANIME, options.boards & WB_BOARD_ANIME);
+
+	res = parse_opt('H', NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_BOARD_HIGHRES, options.boards & WB_BOARD_HIGHRES);
+
+	resetOptions();
+	res = parse_opt(WB_KEY_PRINT_ONLY, NULL, &options);
+	TEST_ASSERT_EQUAL_INT(0, res);
+	TEST_ASSERT_EQUAL_INT(WB_FLAG_PRINT_ONLY, options.flags & WB_FLAG_PRINT_ONLY);
+}
+
+/* Main */
 int main(int argc, char *argv[]) {
 	Unity.TestFile=__FILE__;
 	UnityBegin();
@@ -116,5 +412,19 @@ int main(int argc, char *argv[]) {
 	RUN_TEST(test_parseOpt_aspectRatio_invalid, __LINE__);
 	RUN_TEST(test_parseOpt_color_valid, __LINE__);
 	RUN_TEST(test_parseOpt_color_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_downloadDir_valid, __LINE__);
+	RUN_TEST(test_parseOpt_downloadDir_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_imageNum_valid, __LINE__);
+	RUN_TEST(test_parseOpt_imageNum_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_password_valid, __LINE__);
+	RUN_TEST(test_parseOpt_query_valid, __LINE__);
+	RUN_TEST(test_parseOpt_resolution_valid, __LINE__);
+	RUN_TEST(test_parseOpt_resolution_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_sort_valid, __LINE__);
+	RUN_TEST(test_parseOpt_sort_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_toplist_valid, __LINE__);
+	RUN_TEST(test_parseOpt_toplist_invalid, __LINE__);
+	RUN_TEST(test_parseOpt_username_valid, __LINE__);
+	RUN_TEST(test_parseOpt_flags, __LINE__);
 	return UnityEnd();
 }
